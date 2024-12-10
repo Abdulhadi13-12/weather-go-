d2213c308e949a73dc465fe822c43effcf15feb9
abdul devoloper <shumaila.anees26@gmail.com>
2024-12-10 20:21:21 +0500

    task done

---
+++ script.js
@@ -0,0 +1,19 @@
+function welcomeAlert() {
+    alert("OMG! the weather is so hot");
+}
+
+function infoAlert() {
+    alert("it's too hot outside !");
+}
+
+function warningAlert() {
+    alert("today the weather is normal!");
+}
+
+function successAlert() {
+    alert("Today's weather is cool");
+}
+
+function errorAlert() {
+    alert("OMG! the weather is so cool");
+}
